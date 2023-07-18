35506dc371d9faee67fc6d16b0fb859a3b7ee1df
Antonio <antonioboksic1603@gmail.com>
2023-07-18 22:37:04 +0200

    tabella "comics" popolata correttamente  tramite seeder con i dati forniti per l'esercizio.

--- laravel-project/database/seeders/DatabaseSeeder.php
+++ laravel-project/database/seeders/DatabaseSeeder.php
@@ -14,11 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // questa parte la aggiungiamo noi
+        $this -> call([
+            ComicTableSeeder :: class
+        ]);
     }
 }
