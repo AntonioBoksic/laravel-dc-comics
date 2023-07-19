c2aeea00fb59c8cbbcd0b81e53f8402069aa8b3b
Antonio <antonioboksic1603@gmail.com>
2023-07-20 00:56:41 +0200

    aggiunto i due bottoni "edit" e "delete" che mi serviranno per accedere alle relative rotte.

--- laravel-project/resources/views/comic/index.blade.php
+++ laravel-project/resources/views/comic/index.blade.php
@@ -12,6 +12,13 @@
         <a href=" {{ route('comic.show', $comic -> id) }} ">
         {{$comic["id"]}}) {{$comic["title"]}}
         </a>
+        <br>
+        <a class="btn btn-primary" href="">
+            EDIT
+        </a>
+        <a class="btn btn-primary" href="">
+            DELETE
+        </a>
     </li>
     @endforeach
 </ul>
